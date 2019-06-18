8615f2de51249323eb06dec4ee62999043f8b04f
Mystilund <development.xplx@gmail.com>
2019-06-19 00:55:12 +0200

    Fix config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Cherry Documentation', // Title for your website.
   tagline: 'The documentation to learn how the framework works',
   url: 'https://lund-org.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/cherry-documentation/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
